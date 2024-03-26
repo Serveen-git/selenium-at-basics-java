f6f873909382269306db731feee77d248c02228d
Serveen-BDM <Serveen-BDM@DESKTOP-VIAOE0I>
2024-03-26 16:25:09 +0530

    add test description

--- SeleniumWedriverManager/src/test/java/WebDriverManager/SampleTest2.java
+++ SeleniumWedriverManager/src/test/java/WebDriverManager/SampleTest2.java
@@ -35,7 +35,7 @@ public class SampleTest2 {
 		driver.quit();
 	}
 	
-	@Test
+	@Test(description = "getting title and current url test")
 	public void getTitle() {
 		System.out.println("Title:" +driver.getTitle());
 		System.err.println("URL:" +driver.getCurrentUrl());
